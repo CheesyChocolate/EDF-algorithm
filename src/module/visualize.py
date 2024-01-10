906da3c5226e8ca727bb3bfc4592965b1180e0a4
CheesyChocolate <dev@behnamlal.xyz>
2024-01-10 14:46:07 +0000

    add(visulize): Add deadline visualization

--- src/module/visualize.py
+++ src/module/visualize.py
@@ -11,10 +11,16 @@ def visualize_schedule(tasks):
     for i, task in enumerate(tasks):
         task_name = task[0]
         arrival_time = task[1]
-        execution_time = task[2]
         completion_time = task[4]
+        deadline = task[3]
 
         ax.barh(task_name, width=completion_time - arrival_time, left=arrival_time, label=f'Task {task_name}', color='g')
+
+        # Marking the deadline points
+        ax.axvline(x=deadline, color='r', linestyle='--', linewidth=1)
+        ax.text(deadline, i + 0.1,
+                f'Deadline: {deadline} \n for Task {task_name}',
+                color='r', va='center')
 
     ax.set_xlabel('Time')
     ax.set_ylabel('Tasks')
