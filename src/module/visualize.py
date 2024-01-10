3973dde15bf75d36d645d977ddb6797535752ef3
CheesyChocolate <dev@behnamlal.xyz>
2024-01-10 14:50:26 +0000

    enhancment(visualize): distinguish between deadline met and missed

--- src/module/visualize.py
+++ src/module/visualize.py
@@ -14,13 +14,21 @@ def visualize_schedule(tasks):
         completion_time = task[4]
         deadline = task[3]
 
-        ax.barh(task_name, width=completion_time - arrival_time, left=arrival_time, label=f'Task {task_name}', color='g')
+        if completion_time <= deadline:
+            deadline_status = 'Deadline Met'
+            color = 'g'  # Green color for meeting the deadline
+        else:
+            deadline_status = 'Deadline Missed'
+            color = 'r'  # Red color for missing the deadline
+
+        ax.barh(task_name, width=completion_time - arrival_time,
+                left=arrival_time, label=f'Task {task_name}', color=color)
 
         # Marking the deadline points
         ax.axvline(x=deadline, color='r', linestyle='--', linewidth=1)
         ax.text(deadline, i + 0.1,
-                f'Deadline: {deadline} \n for Task {task_name}',
-                color='r', va='center')
+                f'Deadline: {deadline} \nStatus: {deadline_status}',
+                color='black', va='center')
 
     ax.set_xlabel('Time')
     ax.set_ylabel('Tasks')
